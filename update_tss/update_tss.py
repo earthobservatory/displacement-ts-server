fad1e3b7d132eaa8266801085b04f96ad39e21a0
torresal <alexander.p.torres@jol.nasa.gov>
2019-07-01 09:18:38 -0700

    modified update_tss.py to use update_verdi()

--- update_tss/update_tss.py
+++ update_tss/update_tss.py
@@ -1,6 +1,7 @@
 from sdscli.adapters.hysds.fabfile import *
 from sdscli.adapters.hysds.fabfile import resolve_role as rr
 from sdscli.conf_utils import get_user_config_path, get_user_files_path
+from sdscli.adapters.hysds.update import update_verdi
 import yaml
 import os
 
@@ -12,7 +13,8 @@ import os
 # define private EC2 IP addresses from .sds/config for time series server host
 context = {}
 this_dir = os.path.dirname(os.path.abspath(__file__))
-sds_cfg = get_user_config_path()
+#sds_cfg = get_user_config_path()
+sds_cfg = os.path.expanduser(os.path.join('~', 'Desktop', 'config.test'))
 if not os.path.isfile(sds_cfg):
     raise RuntimeError("SDS configuration file doesn't exist. Run 'sds configure'.")
 with open(sds_cfg) as f:
@@ -34,7 +36,9 @@ def update_ts_server():
     # updates leaflet time series server with displacement-ts-server repo
     role, hysds_dir, hostname = resolve_role()
     if role == 'leaflet_serv':
-        rm_rf('%s/ops/displacement-ts-server' % hysds_dir)
+        sudo_rm_rf('%s/ops/displacement-ts-server' % hysds_dir)
+    update_verdi(context)
+    if role == 'leaflet_serv':
         rsync_project('%s/ops/' % hysds_dir, '~/mozart/ops/displacement-ts-server',
                       ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no")
         cp_rp('~/verdi/ops/displacement-ts-server/verdi_configs/supervisord.conf', '~/verdi/etc/') 
