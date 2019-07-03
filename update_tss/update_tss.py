1dcd465a6b39644e78631206f6d4b77e62dbea72
gmanipon <gmanipon@jpl.nasa.gov>
2019-07-03 08:38:41 +0000

    successfully updated tss with verdi update

--- update_tss/update_tss.py
+++ update_tss/update_tss.py
@@ -2,6 +2,7 @@ from sdscli.adapters.hysds.fabfile import *
 from sdscli.adapters.hysds.fabfile import resolve_role as rr
 from sdscli.conf_utils import get_user_config_path, get_user_files_path
 from sdscli.adapters.hysds.update import update_verdi
+from fabric.contrib.files import exists
 import yaml
 import os
 
@@ -14,7 +15,7 @@ import os
 context = {}
 this_dir = os.path.dirname(os.path.abspath(__file__))
 #sds_cfg = get_user_config_path()
-sds_cfg = os.path.expanduser(os.path.join('~', '.sds', 'config.test'))
+sds_cfg = os.path.expanduser(os.path.join('~', '.sds', 'tss_config'))
 if not os.path.isfile(sds_cfg):
     raise RuntimeError("SDS configuration file doesn't exist. Run 'sds configure'.")
 with open(sds_cfg) as f:
@@ -22,7 +23,7 @@ with open(sds_cfg) as f:
 
 ts_host = '%s' % context['TS_PVT_IP']
 env.roledefs['leaflet_serv'] = [ts_host]
-
+env.roledefs['verdi'] = [ts_host]
 
 def resolve_role():
     # assign hysds_dir to leaflet serv role
@@ -36,8 +37,9 @@ def update_ts_server():
     # updates leaflet time series server with displacement-ts-server repo
     role, hysds_dir, hostname = resolve_role()
     if role == 'leaflet_serv':
-        #sudo_rm_rf('%s/ops/displacement-ts-server' % hysds_dir)
-    update_verdi(context)
+        if exists('/home/verdi/ops/displacement-ts-server') is True:    
+            sudo_rm_rf('%s/ops/displacement-ts-server' % hysds_dir)
+        update_verdi(context)
     if role == 'leaflet_serv':
         rsync_project('%s/ops/' % hysds_dir, '~/mozart/ops/displacement-ts-server',
                       ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no")
